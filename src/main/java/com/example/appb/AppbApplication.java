25cd0806abd02b0f5727e88d0410f7611842f7fd
Kapi44 <116714341+Kapi44@users.noreply.github.com>
2025-07-06 21:10:44 +0200

    Create AppbApplication.java

---
+++ src/main/java/com/example/appb/AppbApplication.java
@@ -0,0 +1,11 @@
+package com.example.appb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppbApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AppbApplication.class, args);
+    }
+}
